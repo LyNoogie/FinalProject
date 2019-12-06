82993b637a25d2f90417be1a0b1bce5a746b9f98
kneeve <kaelin.hoang@gmail.com>
2019-12-06 15:36:16 -0700

    Add high scores functionality (not displaying)

---
+++ KKGame/KKGame/Controllers/HighScoresController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KKGame.Models;
+
+namespace KKGame.Controllers
+{
+    public class HighScoresController : Controller
+    {
+        private readonly GameContext _context;
+
+        public HighScoresController(GameContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HighScores
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.HighScores.ToListAsync());
+        }
+
+        // GET: HighScores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var highScores = await _context.HighScores
+                .FirstOrDefaultAsync(m => m.HighScoreID == id);
+            if (highScores == null)
+            {
+                return NotFound();
+            }
+
+            return View(highScores);
+        }
+
+        // GET: HighScores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HighScores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("HighScoreID,Username,Score,Date")] HighScores highScores)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(highScores);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(highScores);
+        }
+
+        // GET: HighScores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var highScores = await _context.HighScores.FindAsync(id);
+            if (highScores == null)
+            {
+                return NotFound();
+            }
+            return View(highScores);
+        }
+
+        // POST: HighScores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("HighScoreID,Username,Score,Date")] HighScores highScores)
+        {
+            if (id != highScores.HighScoreID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(highScores);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HighScoresExists(highScores.HighScoreID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(highScores);
+        }
+
+        // GET: HighScores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var highScores = await _context.HighScores
+                .FirstOrDefaultAsync(m => m.HighScoreID == id);
+            if (highScores == null)
+            {
+                return NotFound();
+            }
+
+            return View(highScores);
+        }
+
+        // POST: HighScores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var highScores = await _context.HighScores.FindAsync(id);
+            _context.HighScores.Remove(highScores);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HighScoresExists(int id)
+        {
+            return _context.HighScores.Any(e => e.HighScoreID == id);
+        }
+
+        /// <summary>
+        /// Creates a dictionary of all high scores and return that back to the game controller
+        /// </summary>
+        /// <param name="game"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public Dictionary<string, int> GetHighScores(string game)
+        {
+            Dictionary<string, int> resultScores = new Dictionary<string, int>();
+            List<HighScores> allScores = _context.HighScores.OrderByDescending(s => s.Score).ToList();
+            int limit = 5;
+            foreach (HighScores score in allScores)
+            {
+                if (limit >= 0)
+                {
+                    resultScores[score.Username] = score.Score;
+                }
+
+                limit--;
+            }
+
+            return resultScores;
+        }
+
+        [HttpPost]
+        public void InsertScore(string username, string score)
+        {
+            HighScores newScore = new HighScores();
+            newScore.Username = username;
+            newScore.Score = int.Parse(score);
+            newScore.Date = DateTime.Now;
+
+            _context.Add(newScore);
+            _context.SaveChanges();
+        }
+    }
+}
